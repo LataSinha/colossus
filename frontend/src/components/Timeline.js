d2216f0c61933f7a554ecd415026a8c7357dd10d
Lata <sinhalata887@gmail.com>
2022-12-08 02:03:30 +0530

    edited timeline

--- frontend/src/components/Timeline.js
+++ frontend/src/components/Timeline.js
@@ -43,6 +43,24 @@ const Timeline = () => {
       const handleRampage = () => {
         navigate('/rampage')
       }
+      const handleKaraoke = () => {
+        navigate('/karaoke')
+      }
+      const handleBeatExplosion = () => {
+        navigate('/beatExplosion')
+      }
+      const handleArtOfYours = () => {
+        navigate('/artOfYours')
+      }
+      const handleConcentrica = () => {
+        navigate('/concentrica')
+      }
+      const handleSurSangam = () => {
+        navigate('/surSangam')
+      }
+      const handleRockTillYouDrop = () => {
+        navigate('/rockTillYouDrop')
+      }
     return (
         <div>
             <Helmet>
@@ -67,17 +85,17 @@ const Timeline = () => {
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;11:00 a.m to 3:00 p.m</i></p>
                  <p style={{display:'flex'}}>4.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleRampage} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Rampage</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;11:00 a.m to 5:30 p.m</i></p>
-                 <p style={{display:'flex'}}>5.&nbsp;&nbsp;&nbsp;&nbsp; <button  className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Karaoke Marathon</button></p>
+                 <p style={{display:'flex'}}>5.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleKaraoke} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Karaoke Marathon</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;10:00 a.m to 1:00 p.m</i></p>
-                 <p style={{display:'flex'}}>6.&nbsp;&nbsp;&nbsp;&nbsp; <button className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Beat Explosion</button></p>
+                 <p style={{display:'flex'}}>6.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleBeatExplosion} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Beat Explosion</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;10:00 a.m to 3:00 p.m</i></p>
                  <p style={{display:'flex'}}>7.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleUmang} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Umang</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;1:30 p.m to 5:00 p.m</i></p>
                  <p style={{display:'flex'}}>8.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleBetfair} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Bidder's Date</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;12:00 p.m to 5:00 p.m</i></p>
-                 <p style={{display:'flex'}}>9.&nbsp;&nbsp;&nbsp;&nbsp; <button className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Art of Yours</button></p>
+                 <p style={{display:'flex'}}>9.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleArtOfYours} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Art of Yours</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;11:00 a.m to 1:00 p.m</i></p>
-                 <p style={{display:'flex'}}>10.&nbsp;&nbsp;&nbsp;&nbsp; <button className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Concentrica</button></p>
+                 <p style={{display:'flex'}}>10.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleConcentrica} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Concentrica</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;2:00 p.m to 3:30 p.m</i></p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
@@ -95,9 +113,9 @@ const Timeline = () => {
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;11:00 a.m to 4:00 p.m</i></p>
                  <p style={{display:'flex'}}>3.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handlePaperPresentation} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Papyrus</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;11:00 a.m to 3:00 p.m</i></p>
-                 <p style={{display:'flex'}}>4.&nbsp;&nbsp;&nbsp;&nbsp; <button  className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Sursangam</button></p>
+                 <p style={{display:'flex'}}>4.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleSurSangam} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Sursangam</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;10:00 a.m to 12:30 p.m</i></p>
-                 <p style={{display:'flex'}}>5.&nbsp;&nbsp;&nbsp;&nbsp; <button  className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Rock Till You Drop</button></p>
+                 <p style={{display:'flex'}}>5.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleRockTillYouDrop} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Rock Till You Drop</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;1:00 p.m to 4:00 p.m</i></p>
                  <p style={{display:'flex'}}>6.&nbsp;&nbsp;&nbsp;&nbsp; <button onClick={handleUmang} className="vertical-timeline-element-title" style={{background:'#000',color:'#fff',height:'30%',width:'80%',padding:'5%',borderRadius:'5px',fontFamily:'Gotham',fontSize:'150%'}}>Umang</button></p>
                  <p className="vertical-timeline-element-subtitle" style={{marginLeft:'5%'}}><i>Time:&nbsp;10:00 a.m to 3:30 p.m</i></p>
